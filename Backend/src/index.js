d6346707b403964e35a14ad2ca6ff28f7b683484
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-04-15 21:11:49 +0530

    backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks')
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
 app.use('/api/v1',authRoutes);
-// app.use('/api/v1',auth);
+app.use('/api/v1',taskRoutes);
+
 
 app.get('/',(req,res)=>{
+    console.log("Listening on port 3000");
     res.json("hello")
 })
 
--- Backend/src/routes/tasks.js
+++ Backend/src/routes/tasks.js
@@ -1,9 +1,55 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const prisma = new PrismaClient();
 
-router.post('create',async(req, res)=>{
-    const 
+router.use(async(req, res, next)=>{
+    const auth = req.header("authorization") || "";
+
+    const user =  jwt.verify(auth,process.env.JWT_SECRET);
+
+
+
+     req.userId = user.id;
+    
+     next();
 })
+
+router.post('/post', async (req,res)=>{
+    
+       const body = req.body;
+       const userId = req.userId;
+       
+       const task = await prisma.task.create({
+            data:{
+                title : body.title,
+                description : body.description,
+                dueDate : body.dueDate,
+                userId : userId
+            }
+       })
+     
+
+       return res.json({
+        id: task.id
+       })
+})
+
+router.get('/post', async (req,res)=>{
+    const tasks = await prisma.task.findMany({
+         where :{
+             userId : req.userId
+         }
+    })
+  
+
+    return res.json(tasks);
+})
+
+router.put('/post/:id',async (req,res)=>{
+    const task = 
+})
+
+module.exports = router;
